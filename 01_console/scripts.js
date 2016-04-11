bce9d2beb679a863d011c7a4ca817af7b2fda5f8
samanthahindes <samantha.hindes@wustl.edu>
2016-04-11 18:20:36 -0500

    write

--- 01_console/scripts.js
+++ 01_console/scripts.js
@@ -1,4 +1,4 @@
 alert("Help me fix this program!");
 alert("Can you get this message to appear?");
 document.write("<h2>My first JavaScript program</h2>");
-document.wrong("I'm practicing 'debugging'.");
+document.write("I'm practicing 'debugging'.");
